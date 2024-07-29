d24021214370bcbf517386664a7a6758d8543366
Hamza Sajid <hamzasajid@Hamzas-Laptop.local>
2024-07-29 14:11:00 +0500

    guessing game done

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,34 @@
 use std::io;
+use rand::Rng;
 fn main() {
     let mut input = String::new();
-    let n = io::stdin().read_line(&mut input).unwrap();
-    println!("{} bytes read", n);
+    // Prompt the user for input
+     println!("Enter a number between 1 and 100:");
+
+    // Read the input
+    io::stdin().read_line(&mut input).unwrap();
+
+    // Parse the input as a number
+    let number: u32 = match input.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("Invalid input. Please enter a number.");
+            return;
+        }
+    };
+
+    //generate random number
+    let mut rng = rand::thread_rng();
+    let rand_num: u32 = rng.gen_range(0..100);
+    println!("Random number: {}", rand_num);
+
+    // Check if the number is within the valid range
+    if number == rand_num {
+        println!("You entered {} and it matched", number);
+    } else if number > rand_num {
+        println!("number is too big than random number");
+    } else if number < rand_num {
+        println!("number is too small than random number");
+    }
     println!("{}", input);
 }
